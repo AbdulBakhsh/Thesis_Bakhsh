deb2d25505fa7aef3080a20f5378802175b84664
Abdulilah Bakhsh <abdulilah@abdulilahs-mbp.attlocal.net>
2017-07-25 19:18:42 -0700

    Edit 7/25

--- abdul_hypothesis_2.R
+++ abdul_hypothesis_2.R
@@ -45,34 +45,23 @@ lm.beta(hyp1.lm)
     ## not significant because F is 0.2297, which is not significant at p < 0.2147.
     ## for it to be significant F has to be less than p 0.05 and our P is p < 0.2147, thus not significant
 
-## confint(hyp1.lm), 
+## confint(hyp1.lm) 
     ## Emotional clarity (B = .09, CI = -.14, .33) and strategy (B = 0.5, CI = -.05, .15)
 
-## Emotional clarity and absence of strategies did not significantly predict binge-eating, R2 = 0.06, F = (2, 52) = 1.51, p = 0.23.  Emotional clarity (B = .09, CI = -.14, .33) and strategy (B = 0.5, CI = -.05, .15) were both positively, but not significantly, associated with binge-eating.
+## Emotional clarity and absence of strategies did not significantly predict binge-eating, R2 = 0.06, F = (2, 52) = 1.51, p = 0.23.  Emotional clarity (B = .09, CI = -0.14, 0.33) and strategy (B = 0.5, CI = -0.05, 0.15) were both positively, but not significantly, associated with binge-eating.
   
   ## R2 is "Multiple R-squared", F is "F-statistic: 1.513 on 2 and 52 DF", P is "p-value: 0.2297", B is "estimate", CI is "confint"
 
 
 ## The results tells us that there is less than 11.78%  chance that an F-ratio this large would happen if the null hupothesis were true. Therefore, we CANNOT conclude that our regression model results in significiantly better prediction of binge eating than if we used the mean valuse of binge eating. In short, the regression model (emotional clarity and absence of strategies) overal does NOT predicts binge eating and is not significant. 
 
-
-#test.lm <- glm(edds78 ~ clarity + strategy, data = bed.df, family = poisson(link = "log"))
-# summary(test.lm)
-
-# here, use all data and include whether or not meeting criteria interacts with predicted DERS Scales  
-hyp1.lm1 <- lm(edds78 ~ clarity*edds56f + strategy*edds56f, data= abdul.df)
-summary(hyp1.lm1) # not significant
-
-
-
 # how about other DERS total - not hypothesized
-hyp1.lm2 <- lm(edds78 ~ derstot, data= abdul.df)
-summary(hyp1.lm2) # significant but 56 don't matter
+hyp1a.lm <- lm(edds78 ~ derstot, data= abdul.df)
+summary(hyp1a.lm) # significant but 56 don't matter
+confint(hyp1a.lm)
 
-## F-statistic: 7.434 on 1 and 80 DF,  p-value: 0.00786
-    ## If P-value is less than 0.5, then it would be significant.
-    ## Our results show that P= 0.00786 which is less than 0.05 thus, it is significant
-    ## the total ders seems to be significant, thus ders total could be significant predictor of binge eating
+## Results show that the overal DERS significantly predicted binge-eating, R2 = 0.08, F = (1, 80) = 7.43, p = 0.007.  DERS total  (B = 0.03, CI = 0.001, 0.05). 
+
 
 # Graphs for hypothesis 1 --------------------------------------
 abdulna.df <- na.omit(bed.df) # get rid of NA for graphing
@@ -90,9 +79,6 @@ graph3 <- ggplot(data = abdulna.df, aes(y=edds78, x=derstot, colour=edds56f)) + 
 graph3 # an association in right direction but edds56 is not inlcuded
 
 
-
-
-
 #----------------- Hypothesis 2 ---------------------------------------
 
 #hypothesis 2: Gender, food restriction, weight-shape over-evaluation, and overall difficulties with emotion regulation are expected to be significant predictors of binge-eating behavior.
@@ -101,8 +87,6 @@ graph3 # an association in right direction but edds56 is not inlcuded
 ## (edds234)= edds2, edds3, edds4 is weight and shape
 
 ## 1. Make Fear varaiable - 2,3,4 (sum up) (wieght-shape)
-
-abdul.df$edds234 <- abdul.df$edds2 + abdul.df$edds3 + abdul.df$edds4
 
 bed.df$edds234 <- bed.df$edds2 + bed.df$edds3 + bed.df$edds4
 
@@ -142,43 +126,48 @@ graph5
 #----------------- Hypothesis 3 ---------------------------------------
 
 #A more hostile self-concept, more hostile levels of affiliation in a close relationship, and higher autonomy in a close relationship will be associated with increased binge-eating behavior regardless of overall positive or negative coping and overall emotion regulation.
-# higher score on affiliation the more friendly; lower less friendly (hostile)
 
+# higher score on affiliation the more friendly; lower less friendly (hostile)
+## afftotal= total autonomy, auttot= overal autonomy, intafftot= self-concept affiliation, intauttot = self-concept autonomy, DCS3= total negative coping , DSS= total positive coping
+
+## overal affiliation
 # create overall affilaition in a close relationship
 bed.df$afftotal <- bed.df$sostaffp + bed.df$sostaffw + bed.df$sosiaffp + bed.df$sosiaffw + bed.df$ssotaffp + bed.df$ssotaffw + bed.df$ssoiaffp + bed.df$ssoiaffw
-
+## overal autonomy
 bed.df$auttot <- bed.df$sostautp + bed.df$sostautw + bed.df$sosiautp + bed.df$sosiautw + bed.df$ssotautp + bed.df$ssotautw + bed.df$ssoiautp + bed.df$ssoiautw
 
 # introject
 bed.df$intafftot <- bed.df$intaffp + bed.df$intaffw # self concept (introject) affiliation
 bed.df$intauttot <- bed.df$intautp + bed.df$intautw # self concept (introject) autonomy
 
-## Close relationship
-hyp3.lm <- lm(edds78 ~  afftotal + auttot + DCS3 +DSS +  derstot, data=bed.df)
-summary(hyp3.lm) # only DCS2 is significant
+
+
+## Close relationship + introject (self-concept)
+hyp3.lm <- lm(edds78 ~  afftotal + auttot + DCS3 + DSS + derstot + intafftot + intauttot, data=bed.df)
+summary(hyp3.lm) # Not significant
 confint(hyp3.lm)
 lm.beta(hyp3.lm)
-   
-# introject (self-concept)
-hyp3a.lm <- lm(edds78 ~  intafftot + intauttot + DCS3 +DSS +  derstot, data=bed.df) 
-summary(hyp3a.lm) # only DCS2 is significant
+
+## Results show that affiliation, autonomy, postive and negative coping, DERS, self-concept affiliation, self-concept autonomy did NOT significantly predict binge-eating R2= 0.16, F (7, 47) = 1.29, p = 0.28.
+## with affiliation "afftotal" (B= -0.001, CI= -0.003, 0.001), autonomy "auttot" (B= 0.0003 , CI= -0.004, 0.004), Total overall dysfunction coping subscale "DCS3" (B= 0.04, CI= -0.02, 0.11), Total overall functional coping subscale "DSS" (B= 0.01, CI= -0.02, 0.04), DERS total (B=  0.01, CI= -0.02, 0.05), self-concept affiliation "intafftot" (B= 0.01, CI= -0.01, 0.03), self-concept autonomy "intauttot" (B= -0.01 , CI= -0.03, 0.01),
+
+## Close relationship
+hyp3a.lm <- lm(edds78 ~  afftotal + auttot + DCS3 +DSS +  derstot, data=bed.df)
+summary(hyp3a.lm) # Not significant
 confint(hyp3a.lm)
 lm.beta(hyp3a.lm)
 
+## Results show that affiliation, autonomy, postive and negative coping, and DERS did NOT significantly predict binge-eating R2= 0.13, F (5, 49) = 1.40, p = 0.24.
+## with affiliation (B= -0.0008, CI= -0.0002, 0.001), autonomy (B= 0.0003 , CI= -0.004, 0.004), Total overall dysfunction coping subscale "DCS3" (B= 0.08, CI= -0.03, 0.11), Total overall functional coping subscale "DSS" (B= 0.02, CI= -0.01, 0.05), DERS total (B=  0.01, CI= -0.02, 0.04)     
+   
+# introject (self-concept)
+hyp3b.lm <- lm(edds78 ~  intafftot + intauttot + DCS3 + DSS +  derstot, data=bed.df) 
+summary(hyp3b.lm) # Not significant
+confint(hyp3b.lm)
+lm.beta(hyp3b.lm)
+## Results show that self-concept affiliation, self-concept autonomy, postive and negative coping, and DERS did NOT significantly predict binge-eating R2= 0.14, F (5, 49) = 1.58, p = 0.18.
+## with self-concept affiliation (B= 0.01, CI= -0.01, 0.03), self-concept autonomy (B= -0.003 , CI= -0.02, 0.01), Total overall dysfunction coping subscale "DCS3" (B= 0.05, CI= -0.02, 0.11), Total overall functional coping subscale "DSS" (B= 0.01, CI= -0.02, 0.05), DERS total (B=  0.02, CI= -0.02, 0.04) 
 
- ## Multiple R-squared:  0.2111,	Adjusted R-squared:  0.1306 
-    ## F-statistic: 2.623 on 5 and 49 DF,  p-value: 0.03528
-    ## the p-value is 0.03528, which is lower than 0.05, thus, significant
-
-  ## Results show that affiliation, autonomy, and DERS significantly predict binge-eating R2= 0.21, F (4, 49) = 2.62, p = 0.04.
-  ## with affiliation (B= -6.237e-06, CI= -0.001, 0.002), autonomy (B= 8.828e-04 , CI= -0.02, 0.02), Total overall dysfunction coping subscale "DCS1" (B= 2.657e-02, CI= -0.12, 0.06), Total overall dysfunction coping subscale "DCS2" (B= 2.720e-01, CI= 0.08, 0.46), DERS total (B=  4.787e-03, CI= -0.26, 0.04)     
-
-    ## for afftotal (Affiliation) Pr(>|t|) = 0.99404, which is greater than 0.05, NOT significant 
-    ## for intaffw (autonomy) Pr(>|t|) = 0.92971, which is greater than 0.05, NOT significant 
-    ## for DCS1 (Total overall dysfunction coping subscale) Pr(>|t|) = 0.54981, which is greater than 0.05, NOT significant 
-    ## for DCS2 (Total overall dysfunction coping) Pr(>|t|) = 0.00578, which is smaller than 0.05, thus it is significant 
-    ## for derstot Pr(>|t|) = 0.75919, which is greater than 0.05, NOT significant 
-    ## reject hypothesis three, the p-value is 0.03528, which is smaller than 0.05, thus, significant. ?
 
 
 
